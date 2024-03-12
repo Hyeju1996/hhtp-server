b7bb0115045a0a47e81acd95f84c7cafee9afbef
helena <helena@helenas-MacBook-Pro.local>
2024-03-12 11:03:23 -0700

    server hhtp

---
+++ server.js
@@ -0,0 +1,26 @@
+"use strict"
+import http from "http";
+
+//DONE: set host and port as varibles
+
+const host = "localhost";
+const port = 3000;
+
+// DONE: Extract server message into it's own function
+const serverMessageListner = (req , res) => {
+    res.writeHead(200);
+    //Render data
+    res.end ("This is better server code running!!!");
+;}
+
+//DONE: create a server
+const server = http.createServer((request,response) => {
+    response.writeHead(200);
+    //Render data
+    response.end("This is a running server!");
+})
+
+//DONE set the server t listen
+server.listen(3000, "localhost", () => {
+    console.log(`server is running on http://l${host}: ${port}`);
+})
